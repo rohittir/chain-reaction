63689a06759a847f1ca9c650aaf342f700d914ca
rohittir <rohit.tir2@gmail.com>
2017-11-13 20:28:38 -0500

    added secure connection ssl self signed certificate

--- mainServer.js
+++ mainServer.js
@@ -1,23 +1,28 @@
 
-const express = require('express')
+/*
+ * SWE 681 Project: Chain Reaction
+ * Authors: Rohit Tirmanwar, Rohitaksh Vanaparthy
+ * Main server file
+**/
+
+// Headers
+const express = require("express")
 const path = require('path')
 const exphbs = require('express-handlebars')
+var https = require('https')
+var fs = require('fs')
 
-
-const app = express()
+// Define Variables
+const app = express();
 const port = 3010
 
-// app.get('/', (request, response) => {
-//   response.send('Hello from Express!')
-// })
+// ssl/tsl secure key and self-signed certificate generated using openssl
+var options = {
+   key  : fs.readFileSync('server.key'),
+   cert : fs.readFileSync('server.crt')
+};
 
-// app.use((err, request, response, next) => {
-//   // log the error, for now just console.log
-//   console.log(err)
-//   response.status(500).send('Something broke!')
-// })
-
-
+// Set the view to express
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
@@ -28,16 +33,29 @@ app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
 
-
+// Empty request from client renders the home.hbs
 app.get('/', (request, response) => {
   response.render('home', {
     name: 'Rohit'
   })
 })
 
-app.listen(port, (err) => {
-  if (err) {
+// Start the server
+https.createServer(options, app).listen(port, function (err) {
+if (err) {
     return console.log('something bad happened', err)
   }
-  console.log(`server is listening on ${port}`)
-})
+   console.log(`server is listening on ${port}`)
+});
+
+
+
+// app.get('/', (request, response) => {
+//   response.send('Hello from Express!')
+// })
+
+// app.use((err, request, response, next) => {
+//   // log the error, for now just console.log
+//   console.log(err)
+//   response.status(500).send('Something broke!')
+// })
