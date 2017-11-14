f57ff30d904422ddba0a1f511b36e8ace1dff8d1
rohittir <rohit.tir2@gmail.com>
2017-11-13 19:40:25 -0500

    nodejs initial setup

---
+++ mainServer.js
@@ -0,0 +1,43 @@
+
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+
+
+const app = express()
+const port = 3010
+
+// app.get('/', (request, response) => {
+//   response.send('Hello from Express!')
+// })
+
+// app.use((err, request, response, next) => {
+//   // log the error, for now just console.log
+//   console.log(err)
+//   response.status(500).send('Something broke!')
+// })
+
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'Rohit'
+  })
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`server is listening on ${port}`)
+})
